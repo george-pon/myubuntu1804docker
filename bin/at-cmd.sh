ff595b738816d7df68fbe2244b057225a109023e
Jun Obama <george@yk.rim.or.jp>
2019-09-24 21:47:50 +0900

    update shell

---
+++ bin/at-cmd.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+#  指定時刻まで待ってコマンドラインのコマンドを実行する
+#
+#  使用例  at-cmd.sh 1830  ls -la
+#
+#  時刻は HHMM 形式で指定する
+#
+
+function f-at-cmd-usage() {
+    echo "at-cmd.sh  time  command ..."
+}
+
+function f-at-cmd() {
+
+    local curtime=
+    local curtime2=
+    local targettime=
+
+    # 引数解析
+    while [ $# -gt 0 ];
+    do
+        if [ x"$targettime"x = x""x ]; then
+            targettime=$1
+            shift
+            continue
+        fi
+
+        break
+    done
+
+    # チェック
+    if [ -z "$targettime" ]; then
+        f-at-cmd-usage
+        return 0
+    fi
+
+    # 時間待ち
+    while true
+    do
+        curtime=$( date +%H%M )
+        curtime2=$( date +%H%M%S )
+        echo -n -e  "\rtarget time : $targettime    current time : $curtime2"
+        if [ x"$curtime"x = x"$targettime"x ] ; then
+            echo " ... found."
+            break
+        fi
+        sleep 20
+    done
+
+    # コマンド実行
+    "$@"
+
+}
+
+f-at-cmd  "$@"
+
+#
+# end of file
+#
