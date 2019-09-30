b4d12aa4f135542d8d4cc22d7fedaa24459c9e05
Jun Obama <fj6782ix@aa.jp.fujitsu.com>
2019-09-30 15:08:14 +0900

    update shell

--- bin/at-cmd.sh
+++ bin/at-cmd.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #
 #  指定時刻まで待ってコマンドラインのコマンドを実行する
+#  使用例  at-cmd.sh  1830  ls -la
 #
-#  使用例  at-cmd.sh 1830  ls -la
+#  時刻の左に日付指定をつけても良い
+#  使用例  at-cmd.sh  051830  ls -la
 #
-#  時刻は HHMM 形式で指定する
+#  時刻は HHMM 形式 or DDHHMM で指定する
 #
 
 function f-at-cmd-usage() {
     echo "at-cmd.sh  time  command ..."
+    echo "    example:  at-cmd.sh  1830   ls -laF    : match HHMM"
+    echo "    example:  at-cmd.sh  011830   ls -laF  : match DDMMHH"
 }
 
 function f-at-cmd() {
@@ -39,9 +43,13 @@ function f-at-cmd() {
     while true
     do
         curtime=$( date +%H%M )
-        curtime2=$( date +%H%M )
-        echo -n -e  "\rtarget time : $targettime    current time : $curtime2    command: $@"
+        curtime2=$( date +%d%H%M )
+        echo -n -e  "\rtarget time : $targettime    current time : $curtime or $curtime2    command: $@"
         if [ x"$curtime"x = x"$targettime"x ] ; then
+            echo " ... found."
+            break
+        fi
+        if [ x"$curtime2"x = x"$targettime"x ] ; then
             echo " ... found."
             break
         fi
