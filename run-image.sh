e21cd7a9a580c04a48396ec1ffd7b67c1e5136cd
Jun Obama <george@yk.rim.or.jp>
2019-12-09 14:33:08 +0900

    modify shell

--- run-image.sh
+++ run-image.sh
@@ -2,9 +2,6 @@
 #
 # test run image
 #
-function docker-run-myubuntu1804docker() {
-    ${WINPTY_CMD} docker run -i -t --rm \
-        -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
-        myubuntu1804docker:latest
-}
-docker-run-myubuntu1804docker
+${WINPTY_CMD} docker run -i -t --rm \
+    -e http_proxy=${http_proxy} -e https_proxy=${https_proxy} -e no_proxy="${no_proxy}" \
+    myubuntu1804docker:latest
